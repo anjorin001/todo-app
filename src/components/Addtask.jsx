87a5767016aa911a322e1cfe3adf4a574d83cd59
anjorin001 <anjorinfavour123@gmail.com>
2025-04-03 14:20:30 +0100

    Added the Editask component

--- src/components/Addtask.jsx
+++ src/components/Addtask.jsx
@@ -4,11 +4,11 @@ import { useContext } from 'react'
 import { useEffect } from 'react'
 const Addtask = ({ currentPage }) => {
     const {tasks, setTasks, AllTasks, setAllTasks, taskValue, setTaskValue, taskDate,setTaskDate,taskTime,setTaskTime } = useContext(Task)
-    const handleSubmit = (e, page, newTaskText, newTaskDate, newTaskTime) => {
+    const handleSubmit = (e, page, TaskText, TaskDate, TaskTime) => {
         e.preventDefault()
         setTasks((prevTasks) => ({
             ...prevTasks,
-            [page]: [...prevTasks[page] || [],  {id: Date.now(), text: newTaskText, date: newTaskDate, time: newTaskTime, completed: false }]
+            [page]: [...prevTasks[page] || [],  {id: Date.now(), text: TaskText, date: TaskDate, time: TaskTime, completed: false }]
         }))
         setTaskValue('');
         setTaskTime('');
