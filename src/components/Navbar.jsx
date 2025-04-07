182c684784b3ffda2f3d4e672cb0257992e01080
akoko3 <aishatayomideolaosun@gmail.com>
2025-04-07 13:16:16 +0100

    Added new pages and updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
       <nav className="navbar">
-        <NavLink to="">My Day</NavLink>
-        <NavLink to="">Important</NavLink>
-        <NavLink to="">Planned</NavLink>
-        <NavLink to="">Assigned To Me</NavLink>
-        <NavLink to="">Tasks</NavLink>
+        <NavLink to="my-day">My Day</NavLink>
+        <NavLink to="important">Important</NavLink>
+        <NavLink to="planned">Planned</NavLink>
+        <NavLink to="assigned-to-me">Assigned To Me</NavLink>
+        <NavLink to="tasks">Tasks</NavLink>
       </nav>
     )
 }
