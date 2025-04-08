17da435c9602f0023f7570864f9550aba4c0847d
anjorin001 <anjorinfavour123@gmail.com>
2025-04-08 13:49:07 +0100

    updated my auth folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
+import LoggedUser from "@/auth/LoggedUser";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-      <nav className="navbar">
-        <NavLink to="my-day">My Day</NavLink>
-        <NavLink to="important">Important</NavLink>
-        <NavLink to="planned">Planned</NavLink>
-        <NavLink to="assigned-to-me">Assigned To Me</NavLink>
-        <NavLink to="tasks">Tasks</NavLink>
-      </nav>
-    )
-}
+  return (
+    <nav className="navbar">
+      <LoggedUser />
+      <NavLink to="my-day">My Day</NavLink>
+      <NavLink to="important">Important</NavLink>
+      <NavLink to="planned">Planned</NavLink>
+      <NavLink to="assigned-to-me">Assigned To Me</NavLink>
+      <NavLink to="tasks">Tasks</NavLink>
+    </nav>
+  );
+};
 
 export default Navbar;
