357879fa5c363abf14c4906436903f8ed3b615e3
Osa227 <olalekansamuelasiyanbi@gmail.com>
2025-04-18 18:48:14 +0100

    mappin done

--- src/pages/Important.jsx
+++ src/pages/Important.jsx
@@ -6,14 +6,36 @@ import { useContext } from 'react';
 export const Important = () => {
    const { tasks } = useContext(Task);
    const page = "task-page2";
+   const lines = Array.from({ length: tasks[page].length + 7 });
+   const today = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <div className='main-content'>
        <main className="task-content">
         <h1 className='heading'>
          <Star className='star'/>  Important
         </h1>
+        <span>{today}</span>
         <div className="task-scroll-container ">
-          {/* Sam map in here! */}
+          { <ul className="task-list">
+            {lines.map((_, i) => (
+              <li key={i} className="task-line">
+                {tasks[page][i] ? (
+                  <>
+                    <p>{tasks[page][i].text}</p>
+                    <p>
+                      {tasks[page][i].date}  {tasks[page][i].time}
+                    </p>
+                  </>
+                ) : (
+                  ""
+                )}
+              </li>
+            ))}
+          </ul>}
         </div>
          {/* ADDTASK INPUT */}
          <div className="bottom-add-task-input">
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -4,15 +4,37 @@ import Addtask from "@/components/Addtask";
 import { useContext } from "react";
 import { Task } from "@/components/TaskContext";
 const Tasks = () => {
-  const { AllTasks } = useContext(Task);
+  const { tasks } = useContext(Task);
   const page = 'alltask-page'
+  const lines = Array.from({ length: tasks[page].length + 7 });
+  const today = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <div className="main-content">
       <main className="task-content">
         <h1 className="heading">
           <House className="house" /> Tasks
         </h1>
-        <div className="task-scroll-container">{/* Sam map in here! */}</div>
+        <span>{today}</span>
+        <div className="task-scroll-container">{ <ul className="task-list">
+            {lines.map((_, i) => (
+              <li key={i} className="task-line">
+                {tasks[page][i] ? (
+                  <>
+                    <p>{tasks[page][i].text}</p>
+                    <p>
+                      {tasks[page][i].date}  {tasks[page][i].time}
+                    </p>
+                  </>
+                ) : (
+                  ""
+                )}
+              </li>
+            ))}
+          </ul>}</div>
         {/* ADDTASK INPUT */}
         <div className="bottom-add-task-input">
           <Addtask currentPage={page} />
